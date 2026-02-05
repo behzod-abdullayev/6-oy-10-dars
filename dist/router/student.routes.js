cb7940626e20bbfac55b92505c7df5b5bd0a757b
behzod07-abdullayev <bekzod2366@gmail.com>
2026-02-05 20:55:05 +0500

    6-oy-10-dars

--- dist/router/student.routes.js
+++ dist/router/student.routes.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 import { addStudent, deleteStudent, getAllStudents, getOneStudent, leftStudent, statistics, UpdateStudent, } from "../controller/student.controller.js";
 const studentRouter = Router();
 studentRouter.get("/get_all_students", getAllStudents);
-studentRouter.get("/get_one_student", getOneStudent);
+studentRouter.get("/get_one_student/:id", getOneStudent);
 studentRouter.post("/add_student", addStudent);
-studentRouter.put("/update_student", UpdateStudent);
-studentRouter.delete("/delete_student", deleteStudent);
-studentRouter.put("/left_students", leftStudent);
+studentRouter.put("/update_student/:id", UpdateStudent);
+studentRouter.delete("/delete_student/:id", deleteStudent);
+studentRouter.put("/left_students/:id", leftStudent);
 studentRouter.get("/statistics", statistics);
 export default studentRouter;
 //# sourceMappingURL=student.routes.js.map
