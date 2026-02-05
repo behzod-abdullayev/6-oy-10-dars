63cd62fc388da2cef5f486f36178622d6eb9179c
behzod07-abdullayev <bekzod2366@gmail.com>
2026-02-05 20:54:35 +0500

    6-oy-10-dars

--- router/student.routes.ts
+++ router/student.routes.ts
@@ -12,11 +12,11 @@ import {
 const studentRouter: Router = Router();
 
 studentRouter.get("/get_all_students", getAllStudents as RequestHandler);
-studentRouter.get("/get_one_student", getOneStudent as RequestHandler);
+studentRouter.get("/get_one_student/:id", getOneStudent as RequestHandler);
 studentRouter.post("/add_student", addStudent as RequestHandler);
-studentRouter.put("/update_student", UpdateStudent as RequestHandler);
-studentRouter.delete("/delete_student", deleteStudent as RequestHandler);
-studentRouter.put("/left_students", leftStudent as RequestHandler);
+studentRouter.put("/update_student/:id", UpdateStudent as RequestHandler);
+studentRouter.delete("/delete_student/:id", deleteStudent as RequestHandler);
+studentRouter.put("/left_students/:id", leftStudent as RequestHandler);
 studentRouter.get("/statistics", statistics as RequestHandler);
 
 export default studentRouter;
